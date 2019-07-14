45f647142d645e0620358687dc6e9a5d8fa75795
Nils Petter Skålerud <np_skalerud@hotmail.com>
2019-07-14 19:46:13 +0200

    Added example files to repo
    Signed-off-by: Nils Petter Skålerud <np_skalerud@hotmail.com>

---
+++ examples/example.cpp
@@ -0,0 +1,42 @@
+#include "DTex/DTex.hpp"
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	auto path = "resources/02.ktx";
+
+	DTex::LoadInfo<DTex::TextureDocument> loadInfo = DTex::LoadFromFile(path);
+
+	if (loadInfo.IsSuccessful() == false)
+	{
+		std::cout << DTex::ToString(loadInfo.GetResultInfo()) << std::endl;
+		std::cout << loadInfo.GetErrorMessage() << std::endl;
+		std::exit(-1);
+	}
+
+	DTex::TextureDocument& texDoc = loadInfo.GetValue();
+
+	std::cout << "Loaded image file was of type " << DTex::ToString(texDoc.GetSourceFileFormat()) << std::endl;
+
+	std::cout << "TextureDocument's internal buffer contains " << texDoc.GetTotalSizeRequired() << " bytes of data." << std::endl;
+
+	const DTex::Dimensions baseDimensions = texDoc.GetBaseDimensions();
+	std::cout << "Base dimensions are " << baseDimensions.width << " x " << baseDimensions.height << " x " << baseDimensions.depth << "." << std::endl;
+
+	if (texDoc.IsCompressed())
+		std::cout << "The loaded image uses a compressed pixel format." << std::endl;
+	else
+		std::cout << "The loaded image uses an uncompressed pixel format." << std::endl;
+
+	std::cout << "The loaded image has pixelformat: " << DTex::ToString(texDoc.GetPixelFormat()) << std::endl;
+
+	std::cout << "The loaded image is in encoded in colorspace " << DTex::ToString(texDoc.GetColorSpace()) << std::endl;
+
+	std::cout << "Amount of mip levels: " << texDoc.GetMipLevelCount() << std::endl;
+
+	std::cout << "Amount of array layers: " << texDoc.GetArrayLayerCount() << std::endl;
+		
+	return 0;
+}
