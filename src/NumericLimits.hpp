38e6c85e428e6688bf3522c48aaa54a27b7db739
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2026-01-03 17:51:07 +0100

    NumericLimits: Fix compile error on AppleClang

--- src/NumericLimits.hpp
+++ src/NumericLimits.hpp
@@ -12,4 +12,11 @@ namespace Texas::detail
 
 	template<>
 	[[nodiscard]] constexpr std::uint64_t maxValue<std::uint64_t>() noexcept { return static_cast<std::uint64_t>(-1); }
+
+	// Temporary workaround
+#if defined(__clang__) && defined(__apple_build_version__)
+	template<>
+	[[nodiscard]] constexpr std::size_t maxValue<std::size_t>() noexcept { return static_cast<std::size_t>(-1); }
+#endif
+
 }
